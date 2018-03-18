5b04045fa554900ab6d575b4b5356b3d687af377
ChandralekhaMukerji <36000250+ChandralekhaMukerji@users.noreply.github.com>
2018-03-18 21:17:55 +0000

    Update Data frame cleaning.R

--- Data frame cleaning.R
+++ Data frame cleaning.R
@@ -6,6 +6,16 @@ library(reshape2)
 library(scales)
 library(gridExtra)
 
+#Cleaning Regional Danish Population Vs Immigrants#
+Danish_population <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/Danish_populationQ17.csv", 
+                              col_names = FALSE)
+Danish_population.clean <- Danish_population %>% 
+  select(-X1, -X2)
+names(Danish_population.clean) <- c("region", "total", "Danes")
+#calculations of % of immigrant population by region#
+Danish_population.clean<- Danish_population.clean %>%
+  mutate(immigrants = total-Danes, ImmigrantPercentage= immigrants/total*100)
+
 #Cleaning of economic activity rate data#
 economic_activity <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/economic_activity.csv", 
     col_names = FALSE)
@@ -18,7 +28,7 @@ economic_activity.clean.melt <- melt(economic_activity.clean, id="region")
 names(economic_activity.clean.melt) <- c("region", "year", "immigrants")
 economic_activity.clean.melt$year <- as.numeric(as.character(economic_activity.clean.melt$year))
 economic_activity.clean1 <- economic_activity.clean.melt
-aggregate(economic_activity.clean1[, 3], list(economic_activity.clean1$region), mean)
+names(economic_activity.mean) <- aggregate(economic_activity.clean1[, 3], list(economic_activity.clean1$region), mean)
 
 #             Group.1     x
 1 Region Hovedstaden 81.13
@@ -39,14 +49,17 @@ Unemployment_benefits.clean.melt <- melt(Unemployment_benefits.clean, id="region
 names(Unemployment_benefits.clean.melt) <- c("region", "year", "benefits")
 Unemployment_benefits.clean.melt$year <- as.numeric(as.character(Unemployment_benefits.clean.melt$year))
 Unemployment_benefits.clean1 <- Unemployment_benefits.clean.melt
-aggregate(Unemployment_benefits.clean1[, 3], list(Unemployment_benefits.clean1$region), mean)
+names(Unemployment_benefits.mean) <- aggregate(Unemployment_benefits.clean1[, 3], list(Unemployment_benefits.clean1$region), mean)
 
-# Group.2       x
+#Group.2       x
 1 Region Hovedstaden 24409.4
 2 Region Midtjylland 15767.8
 3 Region Nordjylland  8133.6
 4 Region Sj<e6>lland  8997.1
-5 Region Syddanmark 14645.2#
+5 Region Syddanmark  14645.2 #
+
+# merge the two datasets
+Danish_population.merge <- merge(Danish_population.clean, Unemployment_benefits.mean, by="region")
 
 #Cleaning of employment rate data#
 employment_rate <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/employment_rate.csv", 
@@ -63,26 +76,3 @@ unemployement.clean <- unemployement %>%
   select(-X1)
 names(unemployement.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
 
-#CLEANING AND REGIONAL SUM OF NON-EU IMMIGRATION#
-
-#Hovedstaden_Non_EU#
-Hovedstaden_Non_EU <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/nonEU Immigration/Hovedstaden_Non-EU.csv", 
-                               col_names = FALSE)
-Hovedstaden_Non_EU.clean <- Hovedstaden_Non_EU %>% 
-  select(-X1)
-names(Hovedstaden_Non_EU.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
-Hovedstaden_Non_EU.clean.melt <- melt(Hovedstaden_Non_EU.clean, id="region")
-names(Hovedstaden_Non_EU.clean.melt) <- c("region", "year", "immigrants")
-Hovedstaden_Non_EU.clean.melt$year <- as.numeric(as.character(Hovedstaden_Non_EU.clean.melt$year))
-Hovedstaden_Non_EU.clean1 <- Hovedstaden_Non_EU.clean.melt
-
-#variations tried#
-
-Hovedstaden_Non_EU.clean1 <- aggregate(cbind(Europe outside EU-28, Africa, North America, South and Central America, Asia, Oceania, Stateless, Unknown)~year,
-             data = Hovedstaden_Non_EU.clean1, FUN ='sum')
-
-Hovedstaden_Non_EU.clean1 <- aggregate(immigrants$Frequency, by=list(Category=region$Category), FUN=sum)
-
-Hovedstaden_Non_EUTotal <- Hovedstaden_Non_EU.clean1 %>% mutate(region_total = sum(immigrants, BY ))
-
-#####
